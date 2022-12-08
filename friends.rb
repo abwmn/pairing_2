3900acc5d388870a371af32b9ea4263dc6b414ff
Andrew Bowman <andrewtbowman@gmail.com>
2022-12-07 20:28:00 -0500

    Add places array

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ['a', 'b', 'c', 'd', 'e']
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ['Virginia', 'New York', 'New Jersey', 'Pennsylvania', 'North Carolina']
+places.push('Ohio')
+puts places
